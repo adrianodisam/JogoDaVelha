56e053a6366699cb323020e0205f3b7d2e0b7ef3
adrianodisam <adrianodisam@gmail.com>
2021-04-07 21:18:08 -0300

    Primeiro envio

---
+++ scripts/interface.js
@@ -0,0 +1,38 @@
+// garantir que o html foi carregado 
+document.addEventListener('DOMContentLoaded',()=>{
+  // pegar todas divs  com a classe posicoes
+  let posicoes = document.querySelectorAll(".posicoes");
+  // para cada posição um evento click 
+  posicoes.forEach((posicao)=>{
+    posicao.addEventListener('click', selecionarQaudrado);
+  })
+
+});
+
+function selecionarQaudrado(evento){
+// posições target o elemento que sofreu a ação
+// posicao o elemento com id
+  let posicoes = evento.target;
+  posicao = posicoes.id;
+ if(colocarSimbolo(posicao)){
+   setTimeout(()=>{
+    var resultado= document.getElementById('vencedor');
+   resultado.innerHTML =`<div id='vencedor'>o vencedor foi o jogador ${vezJogador}</div>`
+     
+   },10);
+
+ }
+ updateSimbolos(posicao);
+}
+
+// pegar todas as posições qual simbolo referente no tabuleiro
+function updateSimbolos(posicao){
+
+    // pegar todas divs  com a classe posicoes
+    let posicoes = document.getElementById(posicao.toString());
+    let simbolo = taboleiro[posicao];
+    posicoes.innerHTML=`<div class = '${simbolo}'></div>`
+    
+    
+}
+
