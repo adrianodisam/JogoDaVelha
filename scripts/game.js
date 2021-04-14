93124f5133c156679d4af282434dc00994be1585
adrianodisam <adrianodisam@gmail.com>
2021-04-13 21:05:09 -0300

    atribuindo nome referente ao ganhador

--- scripts/game.js
+++ scripts/game.js
@@ -1,58 +1,58 @@
 //inicia as variáveis
 
 //taboleiro no array com 9 posições um para cada quadrado
-let taboleiro = ['','','','','','','','',''];
+let taboleiro = ['', '', '', '', '', '', '', '', ''];
 // vez  do jogador
 let vezJogador = 0;
 // simbolos do quadrado
 let simbolos = ['o', 'x'];
 // fim de jogo
 let gameOver = false;
- let winsStates = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6],
-    ];
-
+let winsStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+let jogador0 = "Bússola";
+let jogador1 = "Despertador";
 
 // função de colocar um simbolo dentro do quadrado
-function colocarSimbolo (posicao){
-  // se é o fim do jogo
-  if(gameOver){
-    return
-  }
-  // verifica se o esapaço do taboleiro está vazio  não atualiza o array
-  if(taboleiro[posicao]== ''){
-// pegar o taboleiro e colocar a posição
-  taboleiro[posicao]= simbolos[vezJogador];
-   gameOver = isWin();
+function colocarSimbolo(posicao) {
+    // se é o fim do jogo
+    if (gameOver) {
+        return
+    }
+    // verifica se o esapaço do taboleiro está vazio  não atualiza o array
+    if (taboleiro[posicao] == '') {
+        // pegar o taboleiro e colocar a posição
+        taboleiro[posicao] = simbolos[vezJogador];
+        gameOver = isWin();
 
-   if(gameOver == false){
-//passar a vez  do jogador
-  vezJogador = (vezJogador ==0) ? 1 : 0;
-  }
-}
+        if (gameOver == false) {
+            //passar a vez  do jogador
+            vezJogador = (vezJogador == 0) ? 1 : 0;
+        }
+    }
     return gameOver;
-  }
+}
 // posições de vitória
-  function isWin(){
-   
-    for(let i = 0; i < winsStates.length; i++){
-      let sequencia =  winsStates[i];
-      let pos1 = sequencia[0];
-      let pos2 = sequencia[1];
-      let pos3 = sequencia[2];
-     if(taboleiro[pos1] == taboleiro[pos2]&&
-        taboleiro[pos1] == taboleiro[pos3]&&
-        taboleiro[pos1] !=''){
-          return true;
+function isWin() {
+
+    for (let i = 0; i < winsStates.length; i++) {
+        let sequencia = winsStates[i];
+        let pos1 = sequencia[0];
+        let pos2 = sequencia[1];
+        let pos3 = sequencia[2];
+        if (taboleiro[pos1] == taboleiro[pos2] &&
+            taboleiro[pos1] == taboleiro[pos3] &&
+            taboleiro[pos1] != '') {
+            return true;
         }
     }
     return false;
-  }
-
+}
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,38 +1,41 @@
 // garantir que o html foi carregado 
-document.addEventListener('DOMContentLoaded',()=>{
-  // pegar todas divs  com a classe posicoes
-  let posicoes = document.querySelectorAll(".posicoes");
-  // para cada posição um evento click 
-  posicoes.forEach((posicao)=>{
-    posicao.addEventListener('click', selecionarQaudrado);
-  })
+document.addEventListener('DOMContentLoaded', () => {
+    // pegar todas divs  com a classe posicoes
+    let posicoes = document.querySelectorAll(".posicoes");
+    // para cada posição um evento click 
+    posicoes.forEach((posicao) => {
+        posicao.addEventListener('click', selecionarQaudrado);
+    })
 
 });
 
-function selecionarQaudrado(evento){
-// posições target o elemento que sofreu a ação
-// posicao o elemento com id
-  let posicoes = evento.target;
-  posicao = posicoes.id;
- if(colocarSimbolo(posicao)){
-   setTimeout(()=>{
-    var resultado= document.getElementById('vencedor');
-   resultado.innerHTML =`<div id='vencedor'>o vencedor foi o jogador ${vezJogador}</div>`
-     
-   },10);
+function selecionarQaudrado(evento) {
+    // posições target o elemento que sofreu a ação
+    // posicao o elemento com id
+    let posicoes = evento.target;
+    posicao = posicoes.id;
+    if (colocarSimbolo(posicao)) {
+        setTimeout(() => {
+            var resultado = document.getElementById('vencedor');
+            if (vezJogador == 0) {
+                resultado.innerHTML = `<div id='vencedor'>o vencedor foi o jogador da ${jogador0}</div>`
+            } else {
+                resultado.innerHTML = `<div id='vencedor'>o vencedor foi o jogador do ${jogador1}</div>`
+            }
+        }, 10);
 
- }
- updateSimbolos(posicao);
+
+    }
+    updateSimbolos(posicao);
 }
 
 // pegar todas as posições qual simbolo referente no tabuleiro
-function updateSimbolos(posicao){
+function updateSimbolos(posicao) {
 
     // pegar todas divs  com a classe posicoes
     let posicoes = document.getElementById(posicao.toString());
     let simbolo = taboleiro[posicao];
-    posicoes.innerHTML=`<div class = '${simbolo}'></div>`
-    
-    
-}
+    posicoes.innerHTML = `<div class = '${simbolo}'></div>`
 
+
+}
